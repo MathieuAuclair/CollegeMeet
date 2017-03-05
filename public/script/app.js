54e48ff7760c051b362cb921a9e1f2888b4f2d00
Mathieu Auclair <mathiass12@hotmail.com>
2017-03-05 09:21:59 -0500

    adding a main page

---
+++ public/script/app.js
@@ -0,0 +1,19 @@
+var signIn = document.getElementById("signup");
+var login = document.getElementById("login");
+
+function displayLogin()
+{
+document.getElementById("titleh2").style.paddingTop = "10px";
+signIn.style.height = "0px";
+login.style.height = "250px";
+}
+
+
+
+function displaySignin()
+{
+document.getElementById("titleh2").style.paddingTop = "10px";
+login.style.height = "0px";
+signIn.style.height = "350px";
+}
+
